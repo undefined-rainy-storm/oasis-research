510839633862d72916169c9a7ffffd5e153e8873
Jonghyeon Park <belline0124@gmail.com>
2024-08-27 05:09:57 +0900

    Add notebooks

--- gmap2json/convert.py
+++ gmap2json/convert.py
@@ -10,7 +10,7 @@ while True:
         result = re.findall(r'([0-9]{1,10}\.[0-9]{1,10})', point_info)
         print(result)
         if result and len(result) == 2:
-            pos.append([*map(float, result)])
+            pos.append({'lat': float(result[0]), 'lng': float(result[1])})
     filename = '.'.join('/'.join(gets.split('/')[1:]).split('.')[:-1]) + '.json'
     with open(f'out/{filename}', mode='w') as f:
         f.write(dumps(pos))
